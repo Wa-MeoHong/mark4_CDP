66c82f598343a1ac150c92060332ed42cb9f14e9
머홍 <deahong1214@gmail.com>
2022-10-30 15:12:29 +0900

    Change FileName

--- Human-Tracking_Mk.4/Human_tracking.sh
+++ Human-Tracking_Mk.4/Human_tracking.sh
@@ -2,7 +2,7 @@
 
 echo "run Human tracking Module"
 
-python /home/pi/Desktop/Human-Tracking_Mk.3/tracking_master.py&
+python /home/pi/Desktop/Human-Tracking_Mk.4/tracking_master.py&
 
 read reply
 
